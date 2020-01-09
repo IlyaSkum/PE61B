17120828f633f4558706860b66a59c59fb6b3c67
Данил <Danil3333@yandex.ru>
2020-01-09 09:23:51 +0500

    Add files via upload

---
+++ practicle one/Naletova/практика1.js
@@ -0,0 +1,8 @@
+    let Anton = (89+120+103)/3;
+    let Ivan = (116+94+123)/3;
+    let Maria = (97+134+105)/3;
+            if ((Anton>Ivan) && (Anton>Maria)) {
+                document.write("Антон набрал больше очков");
+            } else if ((Ivan>Anton) && (Ivan>Maria)) {
+                document.write("Иван набрал больше очков");
+            } else document.write("Мария набрала больше очков");
